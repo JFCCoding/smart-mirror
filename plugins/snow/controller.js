d1287ccbe7f9f245e6cb5d8142aa47aa78d07463
Jake Folsom <xpmcoding@gmail.com>
2017-01-23 20:14:09 -0500

    Updated voice commands

--- plugins/snow/controller.js
+++ plugins/snow/controller.js
@@ -1,13 +1,11 @@
 function snow_storm($scope, $http, SpeechService, Focus) {
 
     //start snow
-    SpeechService.addCommand('snow_storm', function (state, action) {
-      $scope.showSnow = true | false
-
+  SpeechService.addCommand('snow_storm', function () {
+        console.debug("Snow Storm!");
     });
 
 }
-
 
 angular.module('SmartMirror')
     .controller('snow_storm', snow_storm);
